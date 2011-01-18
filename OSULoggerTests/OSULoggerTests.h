c4fc6ac36a99709720054793fd37966efdcc6a2b
William Dillon <hpux735@gmail.com>
2011-01-18 15:13:23 -0800

    Initial Commit

---
+++ OSULoggerTests/OSULoggerTests.h
@@ -0,0 +1,17 @@
+//
+//  OSULoggerTests.h
+//  OSULoggerTests
+//
+//  Created by William Dillon on 1/18/11.
+//  Copyright 2011 Oregon State University (COAS). All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface OSULoggerTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ OSULoggerTests/OSULoggerTests.m
@@ -0,0 +1,26 @@
+//
+//  OSULoggerTests.m
+//  OSULoggerTests
+//
+//  Created by William Dillon on 1/18/11.
+//  Copyright 2011 Oregon State University (COAS). All rights reserved.
+//
+
+#import "OSULoggerTests.h"
+
+
+@implementation OSULoggerTests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+@end
